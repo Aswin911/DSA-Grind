e54bfd68f52a24802f574d1898794f3fae128475
SREEASWIN R <150098028+Aswin911@users.noreply.github.com>
2025-12-04 23:19:35 +0530

    Time: 38 ms (83.61%) | Memory: 17.9 MB (82.93%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -1,26 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subroot: Optional[TreeNode]) -> bool:
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
+            return True
         if not root:
             return False
-        if not subroot:
-            return True
-        
-        if self.isSameTree(root, subroot):
-            return True
-        
-        return self.isSubtree(root.left, subroot) or self.isSubtree(root.right, subroot)
 
-
-    def isSameTree(self, root: Optional[TreeNode], subroot: Optional[TreeNode]) -> bool:
-        # Case 1: both None
-        if not root and not subroot:
+        if self.sameTree(root, subRoot):
             return True
-        # Case 2: one None, one not None → mismatch
-        if not root or not subroot:
-            return False
-        # Case 3: values mismatch
-        if root.val != subroot.val:
-            return False
-        
-        # Case 4: compare children
-        return self.isSameTree(root.left, subroot.left) and self.isSameTree(root.right, subroot.right)
+        return (self.isSubtree(root.left, subRoot) or
+               self.isSubtree(root.right, subRoot))
+
+    def sameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root and not subRoot:
+            return True
+        if root and subRoot and root.val == subRoot.val:
+            return (self.sameTree(root.left, subRoot.left) and
+                   self.sameTree(root.right, subRoot.right))
+        return False
