2140f60928fe87c1506230e3c5bf3d140bdf9cc3
SREEASWIN R <150098028+Aswin911@users.noreply.github.com>
2025-12-08 10:08:47 +0530

    Time: 163 ms (26.49%) | Memory: 22.6 MB (32.79%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -1,11 +1,25 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        max_score = score = 0
-        best_hour = -1
+        n = len(customers)
 
-        for i, c in enumerate(customers):
-            score += 1 if c == 'Y' else -1
-            if score > max_score:
-                max_score, best_hour = score, i
-                
-        return best_hour + 1
+        prefix_n = [0]*(n+1)
+        postfix_y = [0]*(n+1)
+
+        for i in range(1,n+1):
+            prefix_n[i] = prefix_n[i-1]
+            if customers[i-1] == "N":
+                prefix_n[i] += 1
+
+        for i in range(n-1,-1,-1):
+            postfix_y[i] = postfix_y[i+1]
+            if customers[i] == "Y":
+                postfix_y[i] += 1
+
+        min_penalty, idx = float("inf") , 0
+
+        for i in range(0,n+1):
+            penalty = prefix_n[i] + postfix_y[i]
+            if penalty < min_penalty:
+                min_penalty = penalty 
+                idx = i
+        return idx
